fce5c8bb1ca8e3dc580049f8208704f6cca35efe
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2024-07-06 23:27:13 -0400

    Project setup

---
+++ src/main/java/com/theaaronrussell/soci/SociApplication.java
@@ -0,0 +1,13 @@
+package com.theaaronrussell.soci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SociApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SociApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theaaronrussell/soci/SociApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theaaronrussell.soci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SociApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
