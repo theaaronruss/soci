ef9fc98fa3105a3bc9fbfb1be39000b8dbec7ef7
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2024-07-09 01:32:45 -0400

    Add a test endpoint

---
+++ src/main/java/com/theaaronrussell/soci/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.theaaronrussell.soci.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "This is a test endpoint";
+    }
+
+}
