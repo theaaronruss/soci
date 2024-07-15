c5451c2b25899f4b3f96eb39363bbc01c2b98253
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2024-07-14 20:58:45 -0400

    Add User entity and UserRepository for accessing users in database

--- src/main/java/com/theaaronrussell/soci/config/SecurityConfig.java
+++ src/main/java/com/theaaronrussell/soci/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.theaaronrussell.soci.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import javax.sql.DataSource;
 
@@ -10,8 +12,9 @@ import javax.sql.DataSource;
 public class SecurityConfig {
 
     @Bean
-    public JdbcUserDetailsManager userDetailsManager(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
+    public InMemoryUserDetailsManager userDetailsManager(DataSource dataSource) {
+        UserDetails user = User.builder().username("user").password("{noop}password").roles("USER").build();
+        return new InMemoryUserDetailsManager(user);
     }
 
 }
