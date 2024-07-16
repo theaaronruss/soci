0fde233ff3f8f3d0256a0e7ebb91c9f42c5f20ca
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2024-07-16 01:01:33 -0400

    Add Post entity and repository

---
+++ src/main/java/com/theaaronrussell/soci/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.theaaronrussell.soci.repository;
+
+import com.theaaronrussell.soci.entity.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+}
