14eeee0296e0954f1a43f155cfc0d28813a12694
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2024-07-17 20:08:06 -0400

    Remove "continue" URL parameter when logging in

--- sql_scripts/insert_test_data.sql
+++ sql_scripts/insert_test_data.sql
@@ -1,8 +1,8 @@
 USE soci;
 INSERT INTO users (username, first_name, last_name, password) VALUES ('mjones', 'Michael', 'Jones', '{noop}password');
 INSERT INTO users (username, first_name, last_name, password) VALUES ('pwhite', 'Peggy', 'White', '{noop}password');
-INSERT INTO roles (username, role) VALUES ('mjones', 'USER');
-INSERT INTO roles (username, role) VALUES ('pwhite', 'USER');
+INSERT INTO roles (username, role) VALUES ('mjones', 'ROLE_USER');
+INSERT INTO roles (username, role) VALUES ('pwhite', 'ROLE_USER');
 INSERT INTO posts (owner, content) VALUES ('mjones', 'This is my first post!');
 INSERT INTO posts (owner, content) VALUES ('mjones', 'This is my second post, still going!');
 INSERT INTO posts (owner, content) VALUES ('pwhite', 'Who saw the game on Friday?');
---
+++ src/main/java/com/theaaronrussell/soci/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.theaaronrussell.soci.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
+        requestCache.setMatchingRequestParameterName(null);
+        httpSecurity.requestCache(cache -> cache.requestCache(requestCache));
+        httpSecurity.authorizeHttpRequests(request -> request.anyRequest().hasRole("USER"));
+        httpSecurity.formLogin(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+}
